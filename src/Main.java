f4753be0fba8eca87f2da9c849cc5ceec559d0a4
Neto test <uzdemir.a@yandex.ru>
2023-12-25 11:39:44 +0400

    new commit

--- src/Main.java
+++ src/Main.java
@@ -2,15 +2,15 @@
 public class Main {
     public static void main(String[] args) {
 
-        int tic_price = 201;
-        System.out.println("Cтоимость билета в рублях: " + tic_price);
+        int ticPrice = 201;
+        System.out.println("Cтоимость билета в рублях: " + ticPrice);
 
-        int count_mil = tic_price / 20;
+        int countMil = ticPrice / 20;
 
-        if (tic_price % 20 != 0) {
+        if (ticPrice % 20 != 0) {
             System.out.println("Внимание! Дробные мили не начисляются.");
         }
 
-        System.out.println("Количество начисленных бонусных миль: " + count_mil);
+        System.out.println("Количество начисленных бонусных миль: " + countMil);
     }
 }
